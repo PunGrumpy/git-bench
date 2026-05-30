63495cf960d174ac0abe374eb4823765b00029eb
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2026-05-31 01:29:53 +0700

    feat(footer): add footer component with license and author information

---
+++ components/sections/footer.tsx
@@ -0,0 +1,16 @@
+export const Footer = () => (
+  <footer className="flex items-center justify-between border-t border-dotted pt-6 text-muted-foreground text-xs">
+    <span>MIT License</span>
+    <span>
+      Made by{" "}
+      <a
+        className="underline decoration-muted-foreground/40 underline-offset-[3px] decoration-dotted decoration-1 hover:decoration-muted-foreground"
+        href="https://www.pungrumpy.com"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Noppakorn Kaewsalabnil
+      </a>
+    </span>
+  </footer>
+);
